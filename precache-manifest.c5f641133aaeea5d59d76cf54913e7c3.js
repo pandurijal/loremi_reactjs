5331600144aa9afc34c5e7da41daa656dd6c6e3e
Pandu Rijal Pasa <pandurijal96@gmail.com>
2019-01-30 12:09:52 +0700

    Updates

---
+++ precache-manifest.c5f641133aaeea5d59d76cf54913e7c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d25357ae0b9234e395b5",
+    "url": "/loremi/static/js/runtime~main.d25357ae.js"
+  },
+  {
+    "revision": "8b3ff0b7367af3dc089e",
+    "url": "/loremi/static/js/main.8b3ff0b7.chunk.js"
+  },
+  {
+    "revision": "c3524c073eb7e15afe92",
+    "url": "/loremi/static/js/1.c3524c07.chunk.js"
+  },
+  {
+    "revision": "8b3ff0b7367af3dc089e",
+    "url": "/loremi/static/css/main.3706eb04.chunk.css"
+  },
+  {
+    "revision": "c3524c073eb7e15afe92",
+    "url": "/loremi/static/css/1.f873cc84.chunk.css"
+  },
+  {
+    "revision": "e4f798c0ab80e4f23fdd72911bf3e783",
+    "url": "/loremi/index.html"
+  }
+];
